3578fdf798aa148fb52b49e0a9890e7c2e1ad6c4
jiangtao <jtaobox@126.com>
2019-12-22 17:51:47 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# one-puppeteer-service
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+const run = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://wufazhuce.com/", {
+    waitUtil: "networkidle2"
+  });
+
+  const text = await page.$eval(".fp-one-cita a", heading => {
+    return heading.innerText;
+  });
+
+  console.log(text);
+
+  page.close();
+};
+
+run();
